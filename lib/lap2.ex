8f0bea83e509471b24c656b849ed6192bea2cb7b
r1ghtwr0ng <rightwrong@protonmail.com>
2023-02-02 13:38:22 +0000

    Gitignore, mixfile, and simple tests

---
+++ lib/lap2.ex
@@ -0,0 +1,9 @@
+defmodule LAP2 do
+  @moduledoc """
+  Documentation for `LAP2`.
+  """
+
+  def test do
+    :ok
+  end
+end
---
+++ test/lap2_test.exs
@@ -0,0 +1,8 @@
+defmodule LAP2Test do
+  use ExUnit.Case
+  doctest LAP2
+
+  test "test" do
+    assert LAP2.test() == :ok
+  end
+end
