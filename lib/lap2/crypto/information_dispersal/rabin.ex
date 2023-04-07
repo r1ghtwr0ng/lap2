2106a1095b78a3d3c569ef836d5392c019d44e70
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-07 16:32:13 +0000

    Fixed padding + completed Rabin's IDA

--- lib/lap2/crypto/information_dispersal/rabin.ex
+++ lib/lap2/crypto/information_dispersal/rabin.ex
@@ -16,11 +16,9 @@ defmodule LAP2.Crypto.InformationDispersal.Rabin do
   """
   @spec split(binary, non_neg_integer, non_neg_integer) :: list(binary)
   def split(data, n, m) do
-    # Pad the data to be a multiple of m
-    padded_data = PKCS7.pad(data, m)
-
-    # Split F into n chunks of size m
-    byte_chunks = padded_data
+    # Pad the data to be a multiple of m and split it into n chunks of size m
+    byte_chunks = data
+    |> PKCS7.pad(m)
     |> :erlang.binary_to_list()
     |> Enum.chunk_every(m)
 
@@ -29,14 +27,26 @@ defmodule LAP2.Crypto.InformationDispersal.Rabin do
 
     # Calculate the shares
     Matrix.matrix_dot_product(vand_matrix, byte_chunks, @prime)
-    |> Enum.map(fn chunk -> :erlang.list_to_binary(chunk); end)
+    |> Enum.with_index(fn chunk, idx -> %{data: :erlang.list_to_binary(chunk), id: idx + 1}; end)
   end
 
-  @spec reconstruct(list(binary), non_neg_integer, non_neg_integer) :: binary
-  def reconstruct(shares, n, m) do
-    # Concatenate the reassembled_chunks and convert them back to the original message
-    byte_chunks = Enum.map(shares, fn share -> :erlang.binary_to_list(share); end)
-    reassembly_matrix = Matix.generate_reassembly_matrix(m, m)
-    Matrix.dot_product(reassembly_matrix, byte_chunks, @prime)
+  @doc """
+  Reconstruct the data from the given shares.
+  Generates the reassembly matrix and performs matrix multiplication with the data in the shares.
+  Finally, the data is un-padded and returned.
+  """
+  @spec reconstruct(list(map)) :: binary
+  def reconstruct(shares) do
+    # Fetch the data from the shares
+    byte_chunks = Enum.map(shares, fn share -> :erlang.binary_to_list(share.data); end)
+
+    # Fetch the ids from the shares and use them to generate the reassembly matrix
+    Enum.map(shares, fn share -> share.id; end)
+    |> Matrix.vandermonde_inverse(@prime)
+    |> Matrix.matrix_product(byte_chunks, @prime)
+    |> Matrix.transpose()
+    |> Enum.concat()
+    |> :erlang.list_to_binary()
+    |> PKCS7.unpad()
   end
 end
