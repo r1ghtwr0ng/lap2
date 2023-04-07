f4a48d9ecd0590a07a5e099d490d88ee9b9bff95
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-07 15:18:44 +0000

    Completed Matrix module

--- lib/lap2/math/matrix.ex
+++ lib/lap2/math/matrix.ex
@@ -6,9 +6,9 @@ defmodule LAP2.Math.Matrix do
   """
 
   @doc """
-  Calculate the dot product of two matrices.
+  Calculate the dot product of two matrices, ensuring the result is within the finite field.
   """
-  @spec matrix_dot_product(list, list, non_neg_integer) :: integer
+  @spec matrix_dot_product(list, list, non_neg_integer) :: non_neg_integer
   def matrix_dot_product(a, b, field_limit) do
     # TODO verify that a and b are the same length
     Enum.map(a, fn a_row ->
@@ -27,7 +27,7 @@ defmodule LAP2.Math.Matrix do
     1..n
     |> Enum.map(fn i ->
       0..(m-1)
-      |> Enum.map(fn j -> :math.pow(i, j) |> round() |> rem(field_limit); end)
+      |> Enum.map(fn j -> :math.pow(i, j) |> round() |> Integer.mod(field_limit); end)
     end)
   end
 
@@ -37,19 +37,49 @@ defmodule LAP2.Math.Matrix do
   """
   @spec vandermonde_inverse(list, non_neg_integer) :: list
   def vandermonde_inverse(basis, field_limit) do
-    # TODO Generate the modular inverse of a matrix
-    basis
+    # Matrix dimensions
+    m = length(basis)
+    el = elementary_symmetric_functions(m, basis, field_limit)
+    denominators = Enum.map(0..(m-1), fn i ->
+      elt = Enum.at(basis, i)
+      Enum.reduce(0..(m-1), 1, fn j, acc ->
+        if i != j do
+          Integer.mod(acc * (elt - Enum.at(basis, j)), field_limit)
+        else
+          acc
+        end
+      end)
+    end)
+    # Function for calculating the sign of a matrix number based on the index
+    sign = fn a -> Bitwise.band(a, 1) * -2 + 1 end
+    numerators = Enum.map(0..(m-1), fn i ->
+      elt = Enum.at(basis, i)
+      Enum.reduce(1..(m-1), [1], fn j, acc ->
+        # Efficient way of calculating the sign without state
+        numerator = Integer.mod(List.first(acc) * elt + sign.(j) * Enum.at(el, j), field_limit)
+        [numerator | acc]
+      end)
+    end)
+    # Calculate the inverse of the denominator
+    Enum.reduce(0..(m-1), %{return: [], inverses: %{}}, fn i, acc ->
+      denominator = Enum.at(denominators, i)
+      {inv, inverses} = get_inverse(denominator, acc.inverses, field_limit)
+      row = Enum.map(Enum.at(numerators, i), fn numerator ->
+        Integer.mod(numerator * inv, field_limit)
+      end)
+      %{return: [row | acc.return], inverses: inverses}
+    end)
+    |> Map.get(:return)
+    |> Enum.reverse()
+    |> transpose()
   end
 
-  def modulo_inverse(element, field_limit) do
-    # TODO Generate the modular inverse of an element
-    {g, x} = extended_gcd(element, field_limit)
-    if g != 1, do: raise "Element #{element} not invertible"
-    rem(x+field_limit, field_limit)
-  end
-
+  @doc """
+  Calculate the sum of the products of all possible subsets of the list l,
+  with a specified size m. Ensure that the result is within the finite field limit.
+  """
   @spec elementary_symmetric_functions(non_neg_integer, list, non_neg_integer) :: list
-  def elementary_symmetric_functions(m, l, p) do
+  def elementary_symmetric_functions(m, l, field_limit) do
     el = List.duplicate(0, length(l) + 1)
     |> List.duplicate(m + 1)
     new_el = Enum.reduce(1..length(l), el, fn j, acc ->
@@ -68,7 +98,7 @@ defmodule LAP2.Math.Matrix do
         end)
       end)
     end)
-    |> Enum.map(fn row -> List.last(row) |> rem(p) end)
+    |> Enum.map(fn row -> List.last(row) |> Integer.mod(field_limit) end)
   end
 
   @doc """
@@ -87,7 +117,7 @@ defmodule LAP2.Math.Matrix do
     Enum.zip(vector_a, vector_b)
     |> Enum.map(fn {a, b} -> a * b; end)
     |> Enum.sum()
-    |> rem(field_limit)
+    |> Integer.mod(field_limit)
   end
 
   # Perform matrix transposition
@@ -99,11 +129,31 @@ defmodule LAP2.Math.Matrix do
   end
 
   # ---- Private Functions ----
+  # Calculate the extended GCD of two numbers, with the x and y values
   @spec extended_gcd(non_neg_integer, non_neg_integer, non_neg_integer, non_neg_integer, non_neg_integer, non_neg_integer) :: {non_neg_integer, non_neg_integer}
   defp extended_gcd(last_remainder, 0, last_x, _, _, _), do: {last_remainder, last_x}
   defp extended_gcd(last_remainder, remainder, last_x, x, last_y, y) do
     quotient   = div(last_remainder, remainder)
-    remainder2 = rem(last_remainder, remainder)
+    remainder2 = Integer.mod(last_remainder, remainder)
     extended_gcd(remainder, remainder2, x, last_x - quotient*x, y, last_y - quotient*y)
+  end
+
+  # Get the modulo inverse of an element, or calculate it if it doesn't exist
+  @spec get_inverse(non_neg_integer, map, non_neg_integer) :: {non_neg_integer, map}
+  defp get_inverse(element, inverses, _) when is_map_key(inverses, element) do
+    {Map.get(inverses, element), inverses}
+  end
+  defp get_inverse(element, inverses, field_limit) do
+    inverse = modulo_inverse(element, field_limit)
+    {inverse, Map.put(inverses, element, inverse)}
+  end
+
+  # Calculate the modulo inverse of an element
+  @spec modulo_inverse(non_neg_integer, non_neg_integer) :: non_neg_integer
+  defp modulo_inverse(element, field_limit) do
+    # TODO Generate the modular inverse of an element
+    {g, x} = extended_gcd(element, field_limit)
+    if g != 1, do: raise "Element #{element} not invertible"
+    Integer.mod(x+field_limit, field_limit)
   end
 end
