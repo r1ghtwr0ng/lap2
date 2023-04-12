d42026a28226c198fc869b05be01bb0571c9cd37
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-12 01:55:53 +0000

    ProtoBuf + RequestHelper update

--- lib/lap2/utils/protobuf/request_helper.ex
+++ lib/lap2/utils/protobuf/request_helper.ex
@@ -5,6 +5,7 @@ defmodule LAP2.Utils.ProtoBuf.RequestHelper do
   """
 
   alias LAP2.Networking.ProtoBuf
+  alias LAP2.Crypto.CryptoManager
 
   # ---- ProtoBuf wrappers ----
   @doc """
@@ -20,6 +21,22 @@ defmodule LAP2.Utils.ProtoBuf.RequestHelper do
   end
 
   @doc """
+  Deserialise an encrypted request struct.
+  """
+  @spec deserialise_encrypted(binary, non_neg_integer, atom) :: {:ok, map} | {:error, any}
+  def deserialise_encrypted(enc_request, proxy_seqm, crypto_mgr_name \\ :crypto_manager)
+  def deserialise_encrypted(%EncryptedRequest{is_encrypted: false, data: data}, _proxy_seq, _crypto_mgr_name) do
+    deserialise(data)
+  end
+  def deserialise_encrypted(enc_request, proxy_seq, crypto_mgr_name) do
+    # Decrypt the request
+    case CryptoManager.decrypt_request(enc_request, proxy_seq, crypto_mgr_name) do
+      {:ok, data} -> deserialise(data)
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @doc """
   Serialise a request struct.
   """
   @spec serialise(Request) :: {:ok, binary} | {:error, any}
@@ -27,6 +44,23 @@ defmodule LAP2.Utils.ProtoBuf.RequestHelper do
     # Serialise the request
     case ProtoBuf.serialise(request) do
       {:ok, data} -> {:ok, IO.iodata_to_binary(data)}
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  @doc """
+  Encrypt a Request struct and serialise to EncryptedRequest struct.
+  """
+  @spec serialise_encrypted(Request, non_neg_integer, atom) :: {:ok, binary} | {:error, any}
+  def serialise_encrypted(request, proxy_seq, crypto_mgr_name \\ :crypto_manager) do
+    # Serialise the request
+    case ProtoBuf.serialise(request) do
+      {:ok, data} ->
+        # Encrypt the request
+        case CryptoManager.encrypt_request(data, proxy_seq, crypto_mgr_name) do
+          {:ok, enc_request} -> ProtoBuf.serialise(enc_request)
+          {:error, reason} -> {:error, reason}
+        end
       {:error, reason} -> {:error, reason}
     end
   end
