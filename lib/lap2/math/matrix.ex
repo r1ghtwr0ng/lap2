e56897ae939e4112a68d0f2ff641a5d2882260d6
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-17 22:53:35 +0000

    Multiplicative modulo inverse lookup optimization

--- lib/lap2/math/matrix.ex
+++ lib/lap2/math/matrix.ex
@@ -6,6 +6,23 @@ defmodule LAP2.Math.Matrix do
   """
   @on_load :load_nif
 
+  # Cached inverse lookup array for finite field Z/257
+  @inverse_lookup [0, 1, 129, 86, 193, 103, 43, 147, 225, 200, 180, 187, 150, 178, 202,
+  120, 241, 121, 100, 230, 90, 49, 222, 190, 75, 72, 89, 238, 101, 195, 60, 199,
+  249, 148, 189, 235, 50, 132, 115, 145, 45, 163, 153, 6, 111, 40, 95, 175, 166,
+  21, 36, 126, 173, 97, 119, 243, 179, 248, 226, 61, 30, 59, 228, 102, 253, 87,
+  74, 234, 223, 149, 246, 181, 25, 169, 66, 24, 186, 247, 201, 244, 151, 165, 210,
+  96, 205, 127, 3, 65, 184, 26, 20, 209, 176, 152, 216, 46, 83, 53, 139, 135, 18,
+  28, 63, 5, 215, 164, 177, 245, 188, 224, 250, 44, 218, 116, 124, 38, 113, 134,
+  159, 54, 15, 17, 158, 140, 114, 220, 51, 85, 255, 2, 172, 206, 37, 143, 117, 99,
+  240, 242, 203, 98, 123, 144, 219, 133, 141, 39, 213, 7, 33, 69, 12, 80, 93, 42,
+  252, 194, 229, 239, 122, 118, 204, 174, 211, 41, 105, 81, 48, 237, 231, 73, 192,
+  254, 130, 52, 161, 47, 92, 106, 13, 56, 10, 71, 233, 191, 88, 232, 76, 11, 108,
+  34, 23, 183, 170, 4, 155, 29, 198, 227, 196, 31, 9, 78, 14, 138, 160, 84, 131,
+  221, 236, 91, 82, 162, 217, 146, 251, 104, 94, 212, 112, 142, 125, 207, 22, 68,
+  109, 8, 58, 197, 62, 156, 19, 168, 185, 182, 67, 35, 208, 167, 27, 157, 136, 16,
+  137, 55, 79, 107, 70, 77, 57, 32, 110, 214, 154, 64, 171, 128, 256]
+
   @spec load_nif :: :ok | {:error, any}
   def load_nif() do
     :erlang.load_nif("./lib/nifs/matrix", 0)
@@ -111,18 +128,17 @@ defmodule LAP2.Math.Matrix do
       end)
 
     # Calculate the inverse of the denominator
-    Enum.reduce(0..(m - 1), %{return: [], inverses: %{}}, fn i, acc ->
+    Enum.reduce(0..(m - 1), [], fn i, acc ->
       denominator = Enum.at(denominators, i)
-      {inv, inverses} = get_inverse(denominator, acc.inverses, field_limit)
+      inv = get_inverse(denominator, field_limit)
 
       row =
         Enum.map(Enum.at(numerators, i), fn numerator ->
           Integer.mod(numerator * inv, field_limit)
         end)
 
-      %{return: [row | acc.return], inverses: inverses}
+      [row | acc]
     end)
-    |> Map.get(:return)
     |> Enum.reverse()
     |> transpose()
   end
@@ -197,7 +213,6 @@ defmodule LAP2.Math.Matrix do
           non_neg_integer
         ) :: {non_neg_integer, non_neg_integer}
   defp extended_gcd(last_remainder, 0, last_x, _, _, _), do: {last_remainder, last_x}
-
   defp extended_gcd(last_remainder, remainder, last_x, x, last_y, y) do
     quotient = div(last_remainder, remainder)
     remainder2 = Integer.mod(last_remainder, remainder)
@@ -205,14 +220,12 @@ defmodule LAP2.Math.Matrix do
   end
 
   # Get the modulo inverse of an element, or calculate it if it doesn't exist
-  @spec get_inverse(non_neg_integer, map, non_neg_integer) :: {non_neg_integer, map}
-  defp get_inverse(element, inverses, _) when is_map_key(inverses, element) do
-    {Map.get(inverses, element), inverses}
+  @spec get_inverse(non_neg_integer, non_neg_integer) :: non_neg_integer
+  defp get_inverse(element, 257) when element < length(@inverse_lookup) do
+    Enum.at(@inverse_lookup, element)
   end
-
-  defp get_inverse(element, inverses, field_limit) do
-    inverse = modulo_inverse(element, field_limit)
-    {inverse, Map.put(inverses, element, inverse)}
+  defp get_inverse(element, field_limit) do
+    modulo_inverse(element, field_limit)
   end
 
   # Calculate the modulo inverse of an element
