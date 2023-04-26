b5c2769b1dd9beea1b0369ff06650170c74be917
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-26 15:38:38 +0000

    Added commitment scheme

--- lib/lap2/crypto/key_exchange/crsdake.ex
+++ lib/lap2/crypto/key_exchange/crsdake.ex
@@ -2,7 +2,15 @@ defmodule LAP2.Crypto.KeyExchange.CRSDAKE do
   use Rustler, otp_app: :lap2, crate: "c_rsdake"
 
   # When your NIF is loaded, it will override this function.
+  @spec prf_gen(non_neg_integer) :: binary
   def prf_gen(_a), do: :erlang.nif_error(:nif_not_loaded)
 
+  @spec prf_eval(binary, binary) :: binary
   def prf_eval(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+
+  @spec commit_gen(binary, binary) :: binary
+  def commit_gen(_s, _r), do: :erlang.nif_error(:nif_not_loaded)
+
+  @spec commit_vrfy(binary, binary, binary) :: boolean
+  def commit_vrfy(_s, _r, _c), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/c_rsdake/src/lib.rs
+++ native/c_rsdake/src/lib.rs
@@ -1,3 +1,4 @@
+use hashcom_rs::{HashCommitmentScheme, SHA256Commitment};
 use cmac::{Cmac, Mac};
 use aes::Aes128;
 
@@ -14,7 +15,41 @@ fn prf_eval(key: Vec<u8>, data: Vec<u8>) -> Vec<u8> {
     result.to_vec()
 }
 
+#[rustler::nif]
+fn commit_gen(s: Vec<i64>, r: Vec<i64>) -> Vec<u8> {
+    // Move the randomness into a u8 array
+    let mut r_arr = [0u8; 4];
+    for i in 0..4 {
+        r_arr[i] = r[i].try_into().expect("Integer casting sheisse exceptions");
+    }
+
+    // Commit string s and randomness r.
+    let party = SHA256Commitment::new(&s, &r_arr);
+    party.commit().expect("Commitment failed")    
+}
+
+#[rustler::nif]
+fn commit_vrfy(s: Vec<i64>, r: Vec<i64>, c: Vec<i64>) -> bool {
+    // Move the randomness into a u8 array
+    let mut r_arr = [0u8; 4];
+    for i in 0..4 {
+        r_arr[i] = r[i].try_into().expect("Integer casting sheisse exceptions");
+    }
+
+    // Move the commitment into a u8 array
+    let mut commit_arr = [0u8; 32];
+    for i in 0..32 {
+        commit_arr[i] = c[i] as u8;
+    }
+
+    // Commit string s and randomness r.
+    let party = SHA256Commitment::new(&s, &r_arr);
+    party.verify(&commit_arr, &s, &r_arr).expect("Commitment verification failed")
+}
+
 rustler::init!("Elixir.LAP2.Crypto.KeyExchange.CRSDAKE", [
     prf_gen,
-    prf_eval
+    prf_eval,
+    commit_gen,
+    commit_vrfy
 ]);
