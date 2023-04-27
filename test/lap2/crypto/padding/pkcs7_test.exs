28ce5c72c7479902bca93e71c1fe0413bad81252
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-27 22:11:29 +0000

    PKCS7 padding test fixes

--- test/lap2/crypto/padding/pkcs7_test.exs
+++ test/lap2/crypto/padding/pkcs7_test.exs
@@ -3,47 +3,75 @@ defmodule LAP2.Crypto.Padding.PKCS7Test do
   use ExUnit.Case
   doctest LAP2.Crypto.Padding.PKCS7
 
-  test "pad/2" do
-    string_1 = "Test string"
-    string_2 = <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255>>
-    string_3 = "0123456789abcdef"
-    len_1 = 8
-    len_2 = 8
-    len_3 = 16
-    # Expected padding results
-    padded_1 = <<84, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103, 5, 5, 5, 5, 5>>
+  describe "pad/2" do
+    test "Test string padding" do
+      string = "Test string"
+      len = 8
+      # Expected padding results
+      padded = <<84, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103, 5, 5, 5, 5, 5>>
 
-    padded_2 =
-      <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255, 8, 8, 8, 8, 8, 8, 8, 8>>
+      assert padded == PKCS7.pad(string, len)
+    end
 
-    padded_3 =
-      <<48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102, 16, 16, 16, 16, 16, 16,
-        16, 16, 16, 16, 16, 16, 16, 16, 16, 16>>
+    test "Test binary padding" do
+      bin = <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255>>
+      len = 8
 
-    # Test padding
-    assert padded_1 == PKCS7.pad(string_1, len_1)
-    assert padded_2 == PKCS7.pad(string_2, len_2)
-    assert padded_3 == PKCS7.pad(string_3, len_3)
+      padded =
+        <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255, 8, 8, 8, 8, 8, 8, 8, 8>>
+
+      # Test padding
+      assert padded == PKCS7.pad(bin, len)
+    end
+
+    test "Test longer string padding" do
+      string = "0123456789abcdef"
+      len = 16
+
+      padded =
+        <<48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102, 16, 16, 16, 16, 16, 16,
+          16, 16, 16, 16, 16, 16, 16, 16, 16, 16>>
+
+      # Test padding
+      assert padded == PKCS7.pad(string, len)
+    end
   end
 
-  test "unpad/1" do
-    # Padded binaries
-    padded_1 = <<84, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103, 5, 5, 5, 5, 5>>
+  describe "unpad/1" do
+    test "Test string unpadding" do
+      # Padded binary
+      padded = <<84, 101, 115, 116, 32, 115, 116, 114, 105, 110, 103, 5, 5, 5, 5, 5>>
 
-    padded_2 =
-      <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255, 8, 8, 8, 8, 8, 8, 8, 8>>
+      # Expected unpadding results
+      string = "Test string"
 
-    padded_3 =
-      <<48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102, 16, 16, 16, 16, 16, 16,
-        16, 16, 16, 16, 16, 16, 16, 16, 16, 16>>
+      # Test padding
+      assert string == PKCS7.unpad(padded)
+    end
 
-    # Expected unpadding results
-    string_1 = "Test string"
-    string_2 = <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255>>
-    string_3 = "0123456789abcdef"
-    # Test padding
-    assert string_1 == PKCS7.unpad(padded_1)
-    assert string_2 == PKCS7.unpad(padded_2)
-    assert string_3 == PKCS7.unpad(padded_3)
+    test "Test binary unpadding" do
+      # Padded binary
+      padded =
+        <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255, 8, 8, 8, 8, 8, 8, 8, 8>>
+
+      # Expected unpadding results
+      bin = <<1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 255, 255, 255, 255, 255, 255>>
+
+      # Test padding
+      assert bin == PKCS7.unpad(padded)
+    end
+
+    test "Test longer string unpadding" do
+      # Padded binary
+      padded =
+        <<48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102, 16, 16, 16, 16, 16, 16,
+          16, 16, 16, 16, 16, 16, 16, 16, 16, 16>>
+
+      # Expected unpadding results
+
+      string = "0123456789abcdef"
+      # Test padding
+      assert string == PKCS7.unpad(padded)
+    end
   end
 end
