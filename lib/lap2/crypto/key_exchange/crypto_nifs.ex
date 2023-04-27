55feda02adb103c2160f425bdf5d6e5eb86e248b
r1ghtwr0ng <rightwrong@protonmail.com>
2023-04-27 19:10:03 +0000

    NIF wrapper type fixes

--- lib/lap2/crypto/key_exchange/crypto_nifs.ex
+++ lib/lap2/crypto/key_exchange/crypto_nifs.ex
@@ -11,57 +11,57 @@ defmodule LAP2.Crypto.KeyExchange.CryptoNifs do
   Generate pseudo-random function (PRF) output.
   The PRF uses AES128-CMAC.
   """
-  @spec prf_eval(binary, binary) :: charlist
+  @spec prf_eval(charlist, charlist) :: charlist
   def prf_eval(_sk, _data), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Generate a hash-based (SHA256) commitment of a message.
   """
-  @spec commit_gen(binary, binary) :: binary
-  def commit_gen(_s, _r), do: :erlang.nif_error(:nif_not_loaded)
+  @spec commit_gen(charlist, charlist) :: charlist
+  def commit_gen(_sk, _rand), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Verify a hash-based (SHA256) commitment.
   """
-  @spec commit_vrfy(binary, binary, binary) :: boolean
-  def commit_vrfy(_s, _r, _c), do: :erlang.nif_error(:nif_not_loaded)
+  @spec commit_vrfy(charlist, charlist, charlist) :: boolean
+  def commit_vrfy(_sk, _rand, _com), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Sign a message using a Spontaneous Anonymous Group (SAG) ring signature.
   """
-  @spec rs_nif_sign(non_neg_integer, charlist(), list(charlist()), charlist()) ::
-    {charlist(), list(charlist()), list(charlist())}
+  @spec rs_nif_sign(non_neg_integer, charlist, list(charlist), charlist) ::
+    {charlist, list(charlist), list(charlist)}
   def rs_nif_sign(_idx, _sk, _ring, _msg), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Verify a Spontaneous Anonymous Group (SAG) ring signature.
   """
-  @spec rs_nif_vrfy(charlist(), list(charlist), list(charlist()), charlist()) :: boolean
+  @spec rs_nif_vrfy(charlist, list(charlist), list(charlist), charlist) :: boolean
   def rs_nif_vrfy(_chal, _ring, _resp, _msg), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Generate a secret and public key pair tuple for
   a Spontaneous Anonymous Group (SAG) ring signature scheme.
   """
-  @spec rs_nif_gen() :: {charlist(), charlist()}
+  @spec rs_nif_gen() :: {charlist, charlist}
   def rs_nif_gen(), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Generate a secret and public key pair tuple for
   a ed25519_dalek signature scheme.
   """
-  @spec standard_signature_gen() :: {charlist(), charlist()}
+  @spec standard_signature_gen() :: {charlist, charlist}
   def standard_signature_gen(), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Sign a message using a ed25519_dalek signature scheme.
   """
-  @spec standard_signature_sign(charlist(), charlist(), charlist()) :: charlist()
+  @spec standard_signature_sign(charlist, charlist, charlist) :: charlist
   def standard_signature_sign(_sk, _pk, _msg), do: :erlang.nif_error(:nif_not_loaded)
 
   @doc """
   Verify a ed25519_dalek signature.
   """
-  @spec standard_signature_vrfy(charlist(), charlist(), charlist()) :: boolean
+  @spec standard_signature_vrfy(charlist, charlist, charlist) :: boolean
   def standard_signature_vrfy(_sig, _pk, _msg), do: :erlang.nif_error(:nif_not_loaded)
 end
